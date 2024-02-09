4045b072c0ee3b4a348862d95fa7c28102c4994a
Deepesh Patil <deepeshvcd6273@gmail.com>
2024-02-09 14:31:57 +0530

    Added Sheel Script to the repository

---
+++ setup.ps1
@@ -0,0 +1,11 @@
+# Start pip installation
+pip install -r .\requirements.txt
+
+# Display completion message
+Write-Host "`nModules installed successfully." -ForegroundColor Green
+Start-Sleep 1
+Write-Host "`nPress Enter to continue... " -ForegroundColor Yellow
+Read-Host
+
+
+
